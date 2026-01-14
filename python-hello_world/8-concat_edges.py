136e9de4eb4ca7896d6fc9b7460c8aa2dc33a853
Dylan Serigba <12297@holbertonstudents.com>
2026-01-14 19:28:30 +0100

    Fixed 8-concat_edges.py file

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"  # noqa: E501
 str = str[39:67] + str[107:112] + str[:6]
 print(f"{str}")
-
