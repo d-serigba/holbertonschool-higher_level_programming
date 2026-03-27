54735bd0a1e520c604e37737bad32cf039266b0d
d-serigba <sanka93777@gmail.com>
2026-03-27 23:56:33 +0100

    task 12

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// On accède à la propriété 'value' de l'objet pour la modifier
+myObject.value = 89;
+
+console.log(myObject);
